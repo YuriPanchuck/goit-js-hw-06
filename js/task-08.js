c42827d811bf41f38781c28fe9b70f49afa8341c
YuriPanchuck <95381273+YuriPanchuck@users.noreply.github.com>
2022-10-02 21:33:07 +0300

    t8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const formRef = document.querySelector(".login-form");
+const formData = {};
+
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    !event.currentTarget.elements.email.value ||
+    !event.currentTarget.elements.password.value
+  ) {
+    alert("Заповніть усі поля");
+  }
+  formData.email = event.currentTarget.elements.email.value;
+  formData.password = event.currentTarget.elements.password.value;
+  event.currentTarget.reset();
+}
+console.log(formData);
